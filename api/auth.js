d3cfadace47d769ab3c36e62916dd89b08013f2a
Xu Li <xl1393@nyu.edu>
2025-11-05 13:53:28 -0500

    Replace custom OAuth with @openlab/vercel-netlify-cms-github package

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,2 @@
-export default function handler(req, res) {
-  const clientId = process.env.OAUTH_CLIENT_ID;
-  const redirectUri = 'https://rcc-website-five.vercel.app/api/callback';
-
-  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo,user`;
-
-  res.redirect(authUrl);
-}
+import auth from '@openlab/vercel-netlify-cms-github/auth';
+export default auth;
